27e9399f61c6cfcdaefe7b96a48968c7116afc34
don-galak <ptagalac@gmail.com>
2024-06-17 20:39:12 +0300

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+from pdfquery import PDFQuery
+
+pdf = PDFQuery('test.pdf')
+pdf.load()
+
+# Use CSS-like selectors to locate the elements
+text_elements = pdf.pq('LTTextLineHorizontal')
+
+# Extract the text from the elements
+text = [t.text for t in text_elements]
+
+print(text)
